0c49276c18f012d698b0ff989eaa998326654978
Alexander, Antony <Alexander.Antony@icims.com>
2023-07-29 11:33:26 +0530

    added field validation

--- src/main/java/com/golden/propertymanagement/controller/UserController.java
+++ src/main/java/com/golden/propertymanagement/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.golden.propertymanagement.dto.UserDTO;
 import com.golden.propertymanagement.service.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/api/v1/user")
 public class UserController {
@@ -19,14 +21,14 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO) {
+	public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserDTO userDTO) {
 		userDTO = userService.register(userDTO);
 
 		return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<UserDTO> loginUser(@RequestBody UserDTO userDTO) {
+	public ResponseEntity<UserDTO> loginUser(@Valid @RequestBody UserDTO userDTO) {
 		userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
 
 		return new ResponseEntity<>(userDTO, HttpStatus.OK);
--- src/main/java/com/golden/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/golden/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,14 +1,27 @@
 package com.golden.propertymanagement.exception;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<ErrorModel>> fieldValidationException(MethodArgumentNotValidException exp) {
+		List<ErrorModel> errorModels = new ArrayList<>();
+		
+		exp.getBindingResult().getFieldErrors().forEach(fe -> {
+			errorModels.add(new ErrorModel(fe.getField(), fe.getDefaultMessage()));
+		});
+		
+		return new ResponseEntity<>(errorModels, HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException exception) {

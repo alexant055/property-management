5b140bcaab15123451289e10b2354b3c73afabf7
Alexander, Antony <Alexander.Antony@icims.com>
2023-07-31 10:22:17 +0530

    added logger

--- src/main/java/com/golden/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/golden/propertymanagement/exception/CustomExceptionHandler.java
@@ -3,6 +3,8 @@ package com.golden.propertymanagement.exception;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CustomExceptionHandler {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<List<ErrorModel>> fieldValidationException(MethodArgumentNotValidException exp) {
 		List<ErrorModel> errorModels = new ArrayList<>();
 		
 		exp.getBindingResult().getFieldErrors().forEach(fe -> {
+			logger.debug("Inside field validation: {} - {}", fe.getField(), fe.getDefaultMessage());
+			logger.info("Inside field validation: {} - {}", fe.getField(), fe.getDefaultMessage());
 			errorModels.add(new ErrorModel(fe.getField(), fe.getDefaultMessage()));
 		});
 		
@@ -24,7 +30,15 @@ public class CustomExceptionHandler {
 	}
 
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException exception) {
-		return new ResponseEntity<>(exception.getErrors(), HttpStatus.BAD_REQUEST);
+	public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException e) {
+		String defaultMessage = "Business Exception: {} - {}";
+		e.getErrors().forEach(error -> {
+			logger.info(defaultMessage, error.getCode(), error.getMessage());
+			logger.debug(defaultMessage, error.getCode(), error.getMessage());
+			logger.warn(defaultMessage, error.getCode(), error.getMessage());
+			logger.error(defaultMessage, error.getCode(), error.getMessage());
+		});
+		
+		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
 	}
 }
